51d8cf593a6bb907713b1e34a52a5ad8337028ee
tianjialiu <tl2581@columbia.edu>
2023-12-29 05:03:18 -0800

    fix naming of cfline10_fstate column

--- R/9 - make_GOFERfinal.R
+++ R/9 - make_GOFERfinal.R
@@ -1,15 +1,6 @@
-# ============================================
-# make_GOFERfinal.R
-# --------------------------------------------
-# combine fireProg, fireLine, fireIg, summary
-# into a single file for all fires
-# --------------------------------------------
-# @author: Tianjia Liu (tliu@ucar.edu)
-# ============================================
-source("/Users/TLiu/Google Drive/My Drive/github/GOFER/R/globalParams.R")
-setwd(tempFolder)
-
-date2str <- function(x) {format(x,"%Y-%m-%d %H:%M:%S")}
+source("/Users/TLiu/Google Drive/My Drive/WestUSFires/scripts/globalParams.R")
+library("rgdal"); library("raster"); library("tidyverse"); library("sf"); library("lemon")
+setwd("/Users/TLiu/Google Drive/My Drive/WestUSFires/GOES/")
 
 for (satMode in c("gofer_combined","gofer_east","gofer_west")) {
   
@@ -93,7 +84,7 @@ for (satMode in c("gofer_combined","gofer_east","gofer_west")) {
                       rflinelen="rfline_km",cflinelen5="cfline5_km",cflinelen10="cfline10_km",
                       cflinelen25="cfline25_km",cflinelen50="cfline50_km",
                       cflinelen75="cfline75_km",cflinelen90="cfline90_km",
-                      rfline_fstate="rfline_fst",cfline5_fstate="cfline5_fst",cfline_fstate="cfline10_fst",
+                      rfline_fstate="rfline_fst",cfline5_fstate="cfline5_fst",cfline10_fstate="cfline10_fst",
                       cfline25_fstate="cfline25_fst",cfline50_fstate="cfline50_fst",
                       cfline75_fstate="cfline75_fst",cfline90_fstate="cfline90_fst",
                       dfarea="dArea_km2",maefspread="maefspread_kmh",awefspread="awefspread_kmh") %>%
@@ -101,7 +92,7 @@ for (satMode in c("gofer_combined","gofer_east","gofer_west")) {
                       farea,fareaPer,fperim,
                       rflinelen,cflinelen5,cflinelen10,
                       cflinelen25,cflinelen50,cflinelen75,cflinelen90,
-                      rfline_fstate,cfline5_fstate,cfline_fstate,
+                      rfline_fstate,cfline5_fstate,cfline10_fstate,
                       cfline25_fstate,cfline50_fstate,cfline75_fstate,cfline90_fstate,
                       dfarea,maefspread,awefspread)
     } else {
@@ -112,7 +103,7 @@ for (satMode in c("gofer_combined","gofer_east","gofer_west")) {
                       farea="area_km2",fareaPer="fareaPer",fperim="perim_km",
                       rflinelen="rfline_km",cflinelen5="cfline5_km",cflinelen10="cfline10_km",
                       cflinelen25="cfline25_km",cflinelen50="cfline50_km",cflinelen75="cfline75_km",
-                      rfline_fstate="rfline_fst",cfline5_fstate="cfline5_fst",cfline_fstate="cfline10_fst",
+                      rfline_fstate="rfline_fst",cfline5_fstate="cfline5_fst",cfline10_fstate="cfline10_fst",
                       cfline25_fstate="cfline25_fst",cfline50_fstate="cfline50_fst",cfline75_fstate="cfline75_fst",
                       dfarea="dArea_km2",maefspread="maefspread_kmh",awefspread="awefspread_kmh") %>%
         dplyr::select(fname,fyear,tUTC,tLocal,tLocalGMT,
@@ -120,42 +111,42 @@ for (satMode in c("gofer_combined","gofer_east","gofer_west")) {
                       farea,fareaPer,fperim,
                       rflinelen,cflinelen5,cflinelen10,
                       cflinelen25,cflinelen50,cflinelen75,
-                      rfline_fstate,cfline5_fstate,cfline_fstate,
+                      rfline_fstate,cfline5_fstate,cfline10_fstate,
                       cfline25_fstate,cfline50_fstate,cfline75_fstate,
                       dfarea,maefspread,awefspread)
     }
   }
   
-  output_folder <- paste0(dataFolder,"/",satMode_final)
+  output_folder <- paste0("../GOFER/",satMode_final)
   ifelse(!file.exists(output_folder),dir.create(output_folder),F)
   
   fireProgShp_list <- do.call(rbind,fireProgShp_list)
-  st_write(fireProgShp_list,paste0(dataFolder,"/",satMode_final,"/",satMode_shortName,"_fireProg.shp"),
+  st_write(fireProgShp_list,paste0("../GOFER/",satMode_final,"/",satMode_shortName,"_fireProg.shp"),
            quiet=T,append=F)
   
   cfireLine_list <- do.call(rbind,cfireLine_list)
-  st_write(cfireLine_list,paste0(dataFolder,"/",satMode_final,"/",satMode_shortName,"_cfireLine.shp"),
+  st_write(cfireLine_list,paste0("../GOFER/",satMode_final,"/",satMode_shortName,"_cfireLine.shp"),
            quiet=T,append=F)
   
   rfireLine_list <- do.call(rbind,rfireLine_list)
-  st_write(fireProgShp_list,paste0(dataFolder,"/",satMode_final,"/",satMode_shortName,"_rfireLine.shp"),
+  st_write(fireProgShp_list,paste0("../GOFER/",satMode_final,"/",satMode_shortName,"_rfireLine.shp"),
            quiet=T,append=F)
   
   fireIg_list <- do.call(rbind,fireIg_list)
   if (length(which(st_geometry_type(fireIg_list) %in% "MULTIPOINT")) > 0) {
     fireIg_list <- st_collection_extract(fireIg_list,"POINT")
   }
-  st_write(fireIg_list,paste0(dataFolder,"/",satMode_final,"/",satMode_shortName,"_fireIg.shp"),
+  st_write(fireIg_list,paste0("../GOFER/",satMode_final,"/",satMode_shortName,"_fireIg.shp"),
            quiet=T,append=F)
   
   fireSummary_list <- do.call(rbind,fireSummary_list)
-  write.csv(fireSummary_list,paste0(dataFolder,"/",satMode_final,"/",satMode_shortName,"_summary.csv"),
+  write.csv(fireSummary_list,paste0("../GOFER/",satMode_final,"/",satMode_shortName,"_summary.csv"),
             row.names=F)
+  
+  scaleVal <- read.csv(paste0(satMode,"/fireStats/scaleVal_",str_replace(satMode_shortName,"GOFER",""),".csv"))
+  write.csv(fireSummary_list,paste0("../GOFER/",satMode_final,"/",satMode_shortName,"_scaleVal.csv"),
+            row.names=F)
+  
+  print(satMode)
 }
 
-# write fireData.csv, matching column names of final GOFER data
-fireDataOutput <- fireData %>% 
-  dplyr::select(FireName,Year,State,Acres,GOES_UTC,local_tz,local_tzGMT) %>%
-  dplyr::rename(fname="FireName",fyear="Year",state="State",acres_official="Acres",GOESIg_UTC="GOES_UTC")
-
-write.csv(fireDataOutput,paste0(dataFolder,"/fireData.csv"),row.names=F)
